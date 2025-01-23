156a3690dff47a7f6e6e68962ff55ac5ef8c8037
averyden <igo408gt@gmail.com>
2025-01-23 13:52:45 +0100

    Add project files.

---
+++ ConversionHelper/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ConversionHelper
+{
+    /* 
+         Converto is the class name for the conversion helper library
+        Coded in C# using .NET we will only be making use of methods here, no properties.
+         */
+    public class Converto
+    {
+    
+
+
+    }
+}
