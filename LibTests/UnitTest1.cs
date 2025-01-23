c28f50267a5fcfd5db1b008ec46ecfd42eaf34f4
averyden <igo408gt@gmail.com>
2025-01-23 14:09:42 +0100

    Set up first unit tests

--- LibTests/UnitTest1.cs
+++ LibTests/UnitTest1.cs
@@ -3,11 +3,17 @@ using ConversionHelper;
 namespace LibTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ConvertoTest
     {
+        Converto c;
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void RGBToHex_ActuallyReturnsTheProperHexCode_ForWhenAllValuesAre255()
         {
+            c = new Converto();
+
+            Assert.AreEqual("FFF", c.RGBToHex(255, 255, 255));
         }
     }
 }
