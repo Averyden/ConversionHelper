66f89dc6872e0e5168f52fb87ce16e62a2ca03a3
averyden <igo408gt@gmail.com>
2025-01-23 14:20:13 +0100

    Change class name to be more intuitive (how would anyone know its called converto just from the namespace?)

--- ConversionHelper/Class1.cs
+++ ConversionHelper/Class1.cs
@@ -4,10 +4,11 @@
          Converto is the class name for the conversion helper library
         Coded in C# using .NET we will only be making use of methods here, no properties.
          */
-    public class Converto
+    public class ConversionHelper
     {
         public string RGBToHex(int red, int green, int blue)
         {
+
             string output = $"{red:X2}{green:X2}{blue:X2}";
             return $"#{output}";
         }
