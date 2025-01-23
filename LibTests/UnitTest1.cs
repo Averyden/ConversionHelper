5f0adedabe5e8b91e826a7913ac567d4e217dbc9
averyden <igo408gt@gmail.com>
2025-01-23 17:36:39 +0100

    Create new unit tests for exception handling

--- LibTests/UnitTest1.cs
+++ LibTests/UnitTest1.cs
@@ -57,7 +57,7 @@ namespace LibTests
         {
             c = new ConversionHelper();
 
-            Assert.AreEqual("(170, 204, 255)", c.HexToRGB("#AACCFF"));
+            Assert.AreEqual("(170, 204, 255)", c.HexToRGB("#ACF"));
         }
 
         [TestMethod]
@@ -68,7 +68,50 @@ namespace LibTests
             Assert.AreEqual("(69, 117, 78)", c.HexToRGB("45754e"));
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void HexToRGBErrorsWhenNothingIsProvided()
+        {
+            c = new ConversionHelper();
 
+            c.HexToRGB("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))] 
+        public void HexToRGBErrorsOnInvalidFormatBecauseHexCodeIsTooShort()
+        {
+            c = new ConversionHelper();
+
+            c.HexToRGB("ff");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void HexToRGBErrorsOnInvalidFormatBecauseHexCodeIsTooShortButNotBelowThreeInLength()
+        {
+            c = new ConversionHelper();
+
+            c.HexToRGB("#ff33");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void HexToRGBErrorsOnInvalidFormatBecauseHexCodeIsTooLong()
+        {
+            c = new ConversionHelper();
+
+            c.HexToRGB("#45754edf");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void HexToRGBErrorsOnInvalidFormatBecauseTheHexCodeContainsALetterNotSupported()
+        {
+            c = new ConversionHelper();
+
+            c.HexToRGB("#UUU");
+        }
 
     }
 }
